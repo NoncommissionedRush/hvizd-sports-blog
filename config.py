5aaf0c136d1f8b6287b7986d4f8726385be14f4b
Viktor Milosavljevic <milosavljevic.viktor@gmail.com>
2021-07-24 21:57:48 +0200

    add sqlalchemy track modifications setting

--- config.py
+++ config.py
@@ -9,8 +9,9 @@ DEFAULT_POST_IMG = "../static/img/default-post-img.jpg"
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ckeditor = CKEditor(app)
 
