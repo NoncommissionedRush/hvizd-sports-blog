7a90e64a847e4fb9f4ff9f5078367c5ece309dc2
Viktor Milosavljevic <milosavljevic.viktor@gmail.com>
2021-07-26 22:59:10 +0200

    add alert to index.js

--- static/client/public/js/index.js
+++ static/client/public/js/index.js
@@ -1,8 +1,15 @@
 const Confirm = {
+
+  types: {
+    YESNO: "confirm",
+    OK: "ok"
+  },
+
   open(options) {
     options = Object.assign(
       {},
       {
+        type: this.types.YESNO,
         message: "",
         yesText: "Áno",
         noText: "Nie",
@@ -12,17 +19,31 @@ const Confirm = {
       options
 ***REMOVED***;
 
-    html = `
-            <div class="confirm">
-                <div class="confirm-window">
-                    <p class="my-1">${options.message}</p>
-                    <button class="btn btn-small btn-white btn-yes">${options.yesText}</button>
-                    <button class="btn btn-small btn-delete btn-no">${options.noText}</button>
-                </div>
-            </div>
-        `;
+    if (options.type == this.types.YESNO){
+      console.log('funguje')
+      html = `
+      <div class="confirm">
+        <div class="confirm-window">
+            <p class="my-1">${options.message}</p>
+            <button class="btn btn-small btn-white btn-yes">${options.yesText}</button>
+            <button class="btn btn-small btn-delete btn-no">${options.noText}</button>
+        </div>
+      </div>
+    `;
+    } else if (options.type == this.types.OK) {
+      html = `
+      <div class="confirm">
+          <div class="confirm-window">
+              <p class="my-1">${options.message}</p>
+              <button class="btn btn-small btn-white btn-yes">${options.yesText}</button>
+          </div>
+      </div>
+    `;
+    }
+
 
     const template = document.createElement("template");
+
     template.innerHTML = html;
 
     const confirmElement = template.content.querySelector(".confirm");
@@ -41,11 +62,12 @@ const Confirm = {
       this._close(confirmElement);
     });
 
-    btnNo.addEventListener("click", () => {
-      options.onNo();
-      this._close(confirmElement);
-    });
-
+    if(btnNo){
+      btnNo.addEventListener("click", () => {
+        options.onNo();
+        this._close(confirmElement);
+      });
+    }
     document.body.appendChild(template.content);
   },
 
@@ -62,6 +84,7 @@ if (btnDeletePost){
     btnDeletePost.addEventListener("click", (e) => {
       e.preventDefault()
       Confirm.open({
+          type: Confirm.types.YESNO,
           message: "Určite chcete zmazať článok?",
           onYes: () => {
               window.location.href = e.target.href;
@@ -75,6 +98,7 @@ if (btnDeleteProfile){
     btnDeleteProfile.addEventListener("click", (e) => {
       e.preventDefault();
       Confirm.open({
+        type: Confirm.types.YESNO,
         message: "Určite chcete zmazať svoj profil?",
         onYes: () => {
           window.location.href = e.target.href;
@@ -88,10 +112,29 @@ if (btnDeleteComment){
     btnDeleteComment.addEventListener("click", (e) => {
       e.preventDefault();
       Confirm.open({
+        type: Confirm.types.YESNO,
         message: "Určite chcete zmazať komentár?",
         onYes: () => {
           window.location.href = e.target.href;
         },
       });
     });
+}
+
+btnForgotPassword = document.querySelector("#btn-forgot-password")
+if (btnForgotPassword) {
+  btnForgotPassword.addEventListener("click", (e) => {
+    e.preventDefault()
+    Confirm.open({
+      type: Confirm.types.OK,
+      message: "Link na zmenu hesla bol odoslaný na váš email",
+      yesText: "Ok",
+      onYes: () => {
+        form = document.querySelector("#request-password-reset-form")
+        form.submit()
+        console.log(form)
+        // window.location.href = e.target.getAttribute("data-href")
+      }
+    });
+  })
 }
